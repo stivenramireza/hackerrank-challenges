66c19b190d73c505e70feb1139498fb3929945c0
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-09-23 16:31:06 -0500

    Switch Case

---
+++ javascript-10-days/2-day/switch.js
@@ -0,0 +1,30 @@
+'use strict'
+
+function getLetter(s) {
+    let letter;
+    switch(s.charAt(0)) {
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+            letter = 'A'
+            break
+        case 'b': case 'c': case 'd': case 'f': case 'g':
+            letter = 'B'
+            break
+        case 'h': case 'j': case 'k': case 'l': case 'm':
+            letter = 'C'
+            break
+        case 'n': case 'p': case 'q': case 'r': case 's': case 't': case 'v': case 'w': case 'x': case 'y': case 'z':
+            letter = 'D'
+            break
+        default:
+            break
+    }
+    return letter;
+}
+
+
+function main() {
+    const s = 'awxya';
+    console.log(getLetter(s));
+}
+
+main()
