a488c4e7bef801044082c57897b699282f2c62b6
Stiven Ramírez Arango <sramirez@anutibara.com>
2020-10-05 15:16:18 -0500

    Try-Catch-Finally

---
+++ javascript-10-days/3-day/try-catch-finally.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function reverseString(s) {
+    try {
+        s = s.split('').reverse().join('')
+    } catch ({ message }) {
+        console.log(message)
+    } finally {
+        console.log(s)
+    }
+}
+
+function main() {
+    const s = '1234'
+    // const s = 1234
+    reverseString(s)
+}
+
+main()
